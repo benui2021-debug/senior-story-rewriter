5b03eb43667697230cf006f60515e0c9ef57b294
benui2021-debug <benui2021@gmail.com>
2025-11-06 15:38:34 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: true }
-}
-export default nextConfig
+  reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      // 필요 시 사용. 당장 없어도 OK
+      allowedOrigins: ['*'],
+    },
+  },
+};
+
+export default nextConfig;
